85b9613433e17e3a98ea1705acfe04560f22a500
hayntopdawg <hayntopdawg@gmail.com>
2016-12-17 23:21:24 +0900

    Fix folder walking

--- photosort.py
+++ photosort.py
@@ -1,4 +1,5 @@
 import os
+import time
 import PIL.Image
 import PIL.ExifTags
 
@@ -20,8 +21,12 @@ def get_exif(filename):
 # TODO: Create test cases for get_min_date
 # Find initial date of photo, return date in YYYY-MM format
 def get_min_date(exif_data):
+    # TODO: put photo/movie check here
     y, m = "", ""
     dates = [v for k, v in exif_data.items() if type(k) is str and "Date" in k]
+    if not dates:
+        # TODO: fix no dates, use time.strftime
+        pass
     for date in dates:
         exif_date, exif_time = date.split(" ")
         temp_y, temp_m, _ = exif_date.split(":")
@@ -30,8 +35,8 @@ def get_min_date(exif_data):
         elif int(y) <= int(temp_y):
             if int(m) < int(temp_m):
                 y, m = temp_y, temp_m
-    print dates
-    print "-".join((y, m))
+    print "\tDates: {0}".format(dates)
+    print "\t{0}".format("-".join((y, m)))
     return "-".join((y, m))
 
 # Check if file or folder parameter exists, return bool
@@ -41,22 +46,33 @@ def exists(path):
 
 if __name__ == "__main__":
     # Print every file and folder in the root directory
-    for dirpath, dirnames, filenames in os.walk(root):
-        #print "Dirpath: {0}".format(dirpath)
-        for dirname in dirnames:
-            print "{0}".format(dirname)
-            i = 0
-            for filename in filenames:
-                if i > 10:
-                    break
-                if filename.lower().endswith(PICTYPE):
-                    print "\t{0}".format(filename)
-                    #img = PIL.Image.open(os.path.join(dirpath, filename))
-                    exif_data = get_exif(os.path.join(dirpath, filename))
-                    date = get_min_date(exif_data)
-                    # TODO: if date is not a folder, create folder
-                    print exists(os.path.join(root, date))
-                    # TODO: if photo/movie name exists in folder, rename photo with next sequential number
-                    # TODO: move photo/movie to folder
-                i += 1
-        break
+    for folderName, subfolders, filenames in os.walk(root):
+        print "The current folder is: {0}".format(folderName)
+
+        for subfolder in subfolders:
+            print "SUBFOLDER OF {0}: {1}".format(folderName, subfolder)
+
+        for filename in filenames:
+            print "FILE INSIDE {0}: {1}".format(folderName, filename)
+
+            if filename.lower().endswith(PICTYPE):
+                exif_data = get_exif(os.path.join(folderName, filename))
+                date = get_min_date(exif_data)
+                # TODO: if date is not a folder, create folder
+                print "\tPath: {0}".format(exists(os.path.join(root, date)))
+                # TODO: if photo name exists in folder, rename photo with next sequential number
+                # TODO: move photo to folder
+
+            elif filename.lower().endswith(MOVTYPE):  # TODO: handle pictures AND movies simultaneously
+                print "\t{0}".format(os.path.join(folderName,filename))
+                secs = os.path.getmtime((os.path.join(folderName, filename)))  # TODO: get_exif may be where to test for pic or movie
+                print "\t\tDate: {0}".format(time.ctime(secs))
+                print "\t\tFormatted: {0}".format(time.strftime("%Y-%m", time.gmtime(secs)))
+                date = time.strftime("%Y-%m", time.gmtime(secs))
+                # TODO: if date is not a folder, create folder
+                print "\t\tPath: {0}".format(exists(os.path.join(root, date)))
+                # TODO: if movie name exists in folder, rename photo with next sequential number
+                # TODO: move movie to folder
+
+            else:
+                print "\tDid not process: {0}".format(filename)
